4ab1505e52eb3a9667dbd2518a519f9ac49af33c
dgduncan <derekgduncan@gmail.com>
2017-11-28 20:46:42 -0600

    added cryptopro_alexa lambda

---
+++ lambda/cryptopro_alexa/index.js
@@ -0,0 +1,88 @@
+// 1. Text strings =====================================================================================================
+ //    Modify these strings and messages to change the behavior of your Lambda function
+
+ let speechOutput;
+ let reprompt;
+ const welcomeOutput = "Let's plan a trip. Where would you like to go?";
+ const welcomeReprompt = "Let me know where you'd like to go or when you'd like to go on your trip";
+ const tripIntro = [
+   "This sounds like a cool trip. ",
+   "This will be fun. ",
+   "Oh, I like this trip. "
+ ];
+
+'use strict';
+const Alexa = require('alexa-sdk');
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const handlers = {
+    'LaunchRequest': function () {
+      this.response.speak(welcomeOutput).listen(welcomeReprompt);
+      this.emit(':responseReady');
+    },
+    'PlanMyTrip': function () {
+        //delegate to Alexa to collect all the required slot values
+        var filledSlots = delegateSlotCollection.call(this);
+
+        //compose speechOutput that simply reads all the collected slot values
+        var speechOutput = randomPhrase(tripIntro);
+
+        //activity is optional so we'll add it to the output
+        //only when we have a valid activity
+        var travelMode = isSlotValid(this.event.request, "travelMode");
+        if (travelMode) {
+          speechOutput += travelMode;
+        } else {
+          speechOutput += "You'll go ";
+        }
+
+        //Now let's recap the trip
+        var fromCity=this.event.request.intent.slots.fromCity.value;
+        var toCity=this.event.request.intent.slots.toCity.value;
+        var travelDate=this.event.request.intent.slots.travelDate.value;
+        speechOutput+= " from "+ fromCity + " to "+ toCity+" on "+travelDate;
+
+        var activity = isSlotValid(this.event.request, "activity");
+        if (activity) {
+          speechOutput += " to go "+ activity;
+        }
+
+        //say the results
+        this.response.speak(speechOutput);
+        this.emit(":responseReady");
+    },
+    'AMAZON.HelpIntent': function () {
+        speechOutput = "";
+        reprompt = "";
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        speechOutput = "";
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        speechOutput = "";
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest': function () {
+        var speechOutput = "";
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+};
+
+
+
+
+
+exports.handler = (event, context) => {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    //alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
